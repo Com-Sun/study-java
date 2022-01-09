47871a2dfa541030bd17231f3fa1ba1838c6fa90
Com-Sun <gnlslfflffl@gmail.com>
2022-01-09 21:27:02 +0900

    study: OOP

---
+++ src/part3/studyoop/ex3/ex3.java
@@ -0,0 +1,37 @@
+package part3.studyoop.ex3;
+
+public class ex3 {
+    public static void main(String[] args) {
+        FarmMachine consum = new FarmMachine(1000000, 2020, "red");
+        System.out.println(String.format("%, d", consum.price));
+        System.out.println(consum.year);
+        System.out.println(consum.color);
+        consum.move();
+        consum.dig();
+        consum.grind();
+
+    }
+}
+
+
+class FarmMachine {
+    int price;
+    int year;
+    String color;
+
+    FarmMachine (int price, int year, String color) {
+        this.price = price;
+        this.year = year;
+        this.color = color;
+    }
+
+    public void move() {
+        System.out.println("Farm-machine is moving");
+    }
+    public void dig() {
+        System.out.println("Farm-machine is digging");
+    }
+    public void grind() {
+        System.out.println("Farm-machine is grinding");
+    }
+}
